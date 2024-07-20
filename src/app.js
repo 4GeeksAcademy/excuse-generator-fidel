5cf91aa0c982dd5d2ddcfbf0acd9625f9d8ed2e4
Fidel  Nieto <nietofidel25@gmail.com>
2024-07-20 13:23:40 +0000

    finishing excuseGenerator, but i wanna make it better

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let paragraph = document.getElementById("excuse");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+const excuses = [who, action, what, when];
+let excuse = "";
+
+function excuseGenerator(arrayOfExcuses) {
+  for (let i = 0; i < arrayOfExcuses.length; i++) {
+    let randomIndex = Math.floor(Math.random() * arrayOfExcuses[i].length);
+    excuse = excuse + " " + arrayOfExcuses[i][randomIndex];
+  }
+  paragraph.textContent = excuse;
+  return paragraph;
+}
+excuseGenerator(excuses);
+
+window.onload = function() {};
