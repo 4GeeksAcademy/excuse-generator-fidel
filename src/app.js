d776f067da296a094ba136fe23bbcda5e08edb91
Fidel  Nieto <nietofidel25@gmail.com>
2024-07-21 13:26:35 +0000

    excuse generator done!

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@
 import "bootstrap";
 import "./style.css";
 
-let paragraph = document.getElementById("excuse");
+const paragraph = document.getElementById("excuse");
+const button = document.getElementById("excuseButton");
 
-let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let who = [
+  "The dog",
+  "My grandma",
+  "The mailman",
+  "My bird",
+  "A man",
+  "Someone",
+  "My mother in law"
+];
 let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
+let what = ["my homework", "my phone", "my car"];
 let when = [
   "before the class",
   "when I was sleeping",
@@ -16,9 +25,10 @@ let when = [
 ];
 
 const excuses = [who, action, what, when];
-let excuse = "";
 
+/* 
 function excuseGenerator(arrayOfExcuses) {
+  let excuse = "";
   for (let i = 0; i < arrayOfExcuses.length; i++) {
     let randomIndex = Math.floor(Math.random() * arrayOfExcuses[i].length);
     excuse = excuse + " " + arrayOfExcuses[i][randomIndex];
@@ -26,6 +36,22 @@ function excuseGenerator(arrayOfExcuses) {
   paragraph.textContent = excuse;
   return paragraph;
 }
+*/
+
+function excuseGenerator(arrayOfExcuses) {
+  let excuse = "";
+  arrayOfExcuses.forEach(array => {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    excuse += " " + array[randomIndex];
+  });
+  paragraph.textContent = excuse;
+  return excuse;
+}
+
 excuseGenerator(excuses);
+
+button.addEventListener("click", function() {
+  excuseGenerator(excuses);
+});
 
 window.onload = function() {};
